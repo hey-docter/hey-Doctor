8ce8da0e86cafcde94c4f3b7ab8e686568399ada
dev-h2s <dev.2esc2esch@gmail.com>
2023-05-10 00:50:45 +0900

    init

---
+++ src/test/java/com/heydocter/heydocter/HeydocterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heydocter.heydocter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeydocterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
