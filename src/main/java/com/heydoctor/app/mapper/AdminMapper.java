bffb43bd4fdb6bc20d9a3a2e629d5f3e257c3458
esther-moon-jym <esther021210@gmail.com>
2023-05-22 00:54:04 +0900

    1:1

--- src/main/java/com/heydoctor/app/mapper/AdminMapper.java
+++ src/main/java/com/heydoctor/app/mapper/AdminMapper.java
@@ -5,17 +5,18 @@ import com.heydoctor.app.domain.vo.QuestionVO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-import java.util.Optional;
 
 @Mapper
-public interface QuestionMapper {
+public interface AdminMapper {
     // 질문 목록 - 검색어 / 10개씩 무한로딩 DTO
-    public List<QuestionVO> selectList(int page);
+    public List<QuestionVO> selectLists(Integer page);
 
     // 질문 상세보기 - 질문에 따른 답변 - 답변에 따른 댓글
-    public Optional<QuestionDTO> selectOne(Long questionId);
+    public QuestionDTO select(Long qnaId);
 
-    //질문 지우기
-    //
-    public void delete(QuestionVO questionVO);
+    // 질문 작성하기
+    // 사용시 id가 주입됨.
+    public void insert(QuestionVO questionVO);
+
+    public void delete(Long qnaId);
 }
--- src/main/java/com/heydoctor/app/mapper/QnaMapper.java
+++ src/main/java/com/heydoctor/app/mapper/QnaMapper.java
@@ -2,6 +2,7 @@ package com.heydoctor.app.mapper;
 
 import com.heydoctor.app.domain.dto.QnaDTO;
 import com.heydoctor.app.domain.vo.QnaVO;
+import com.heydoctor.app.domain.vo.QuestionVO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -10,11 +11,14 @@ import java.util.Optional;
 @Mapper
 public interface QnaMapper {
     // 질문 목록 - 검색어 / 10개씩 무한로딩 DTO
-    public List<QnaVO> selectList(int page);
+    public List<QnaDTO> selectList(int page);
 
     // 질문 상세보기 - 질문에 따른 답변 - 답변에 따른 댓글
     public Optional<QnaDTO> selectOne(Long qnaId);
 
+    //게시글 삭제
+    public void delete(int qnaId);
 
-    public void delete(QnaVO qnaVO);
+    //게시글 삭제
+    public void insert(QnaVO qnaVO);
 }
