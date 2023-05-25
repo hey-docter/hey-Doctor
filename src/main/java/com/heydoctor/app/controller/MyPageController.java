13e2ac0c95ed6bbab1062c403ff0f27f5b89a437
dev-h2s <dev.2esc2esch@gmail.com>
2023-05-26 06:20:01 +0900

    회원 정보 불러오기

--- src/main/java/com/heydoctor/app/controller/MyPageController.java
+++ src/main/java/com/heydoctor/app/controller/MyPageController.java
@@ -20,70 +20,87 @@ import java.util.List;
 @RequestMapping("/mypage/*")
 public class MyPageController {
     private final MyPageService myPageService;
-
+    private final HttpSession session;
     //community 작성한 글, 댓글 보기
     @GetMapping("community")
-    public void community(Long id, Model model) {
+    public void community(Long id, Model model, HttpSession session) {
         model.addAttribute("posts", myPageService.getQuestionList(1L));
-
-//        model.addAttribute("memberName", memberService.getMember((Long)session.getAttribute("id")).get().getMemberName());
+//      회원 정보 가져오기
+        model.addAttribute("user", myPageService.getUser(1L));
     }
+
+
 
     // 답변
     @GetMapping("community-answer")
-    public void communityAnswer(Long id, Model model) {
+    public void communityAnswer(Long id, Model model, HttpSession sessionID ) {
         model.addAttribute("posts", myPageService.getAnswerList(1L));
+        //      회원 정보 가져오기
+        model.addAttribute("user", myPageService.getUser(1L));
     }
 
     // 댓글
     @GetMapping("community-reply")
-    public void communityReply(Long id, Model model) {
+    public void communityReply(Long id, Model model, HttpSession session ) {
         model.addAttribute("posts", myPageService.getReplyList(1L));
+        //      회원 정보 가져오기
+        model.addAttribute("user", myPageService.getUser(1L));
     }
+
 
 
     //스테이터스
     @GetMapping("essay")
-    public void essay() {
+    public void essay(Long id, Model model, HttpSession session ) {
+        //      회원 정보 가져오기
+        model.addAttribute("user", myPageService.getUser(1L));
         log.info("essay");
     }
 
     // 회원탈퇴
     @GetMapping("quit")
-    public void quit() {
+    public void quit(Long id, Model model, HttpSession session ) {
+        //      회원 정보 가져오기
+        model.addAttribute("user", myPageService.getUser(1L));
         log.info("quit");
     }
 
     //예약목록
     @GetMapping("reservation-list")
-    public void reservationList() {
+    public void reservationList(Long id, Model model, HttpSession session ) {
+        //      회원 정보 가져오기
+        model.addAttribute("user", myPageService.getUser(1L));
         log.info("reservation-list");
     }
 
     //개인 정보 설정
     @GetMapping("setting")
-    public void setting() {
+    public void setting(Long id, Model model, HttpSession session ) {
+        //      회원 정보 가져오기
+        model.addAttribute("user", myPageService.getUser(1L));
         log.info("setting");
     }
 
     //구독관리
     @GetMapping("subscribe")
-    public void subscribe() {
+    public void subscribe(Long id, Model model, HttpSession session ) {
+        //      회원 정보 가져오기
+        model.addAttribute("user", myPageService.getUser(1L));
         log.info("subscribe");
     }
 
     //이력 작성(마이페이지에서 이력 클릭시 이동)
     @GetMapping("resume")
-    public void resume() {
+    public void resume(Long id, Model model, HttpSession session ) {
         log.info("resume");
     }
 
     //이력 목록
     @GetMapping("resume-control")
-    public void resumeControl(){log.info("resume-control");}
+    public void resumeControl(Long id, Model model, HttpSession session ){log.info("resume-control");}
 
     //이력 수정
     @GetMapping("resume-edit")
-    public void resumeEdit(){log.info("resume-edit");}
+    public void resumeEdit(Long id, Model model, HttpSession session ){log.info("resume-edit");}
 }
 
