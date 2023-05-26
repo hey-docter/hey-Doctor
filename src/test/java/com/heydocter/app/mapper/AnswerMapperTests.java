47ecaee0807b12aa0799742a7fdf2f0ffdf460da
exceptionError-jin <kj98224849@gamil.com>
2023-05-27 07:17:10 +0900

    테스트 수정

--- src/test/java/com/heydocter/app/mapper/AnswerMapperTests.java
+++ src/test/java/com/heydocter/app/mapper/AnswerMapperTests.java
@@ -34,7 +34,7 @@ public class AnswerMapperTests {
         answerVO.setAnswerIsPublic(true);
         answerVO.setQuestionId(6L);
         answerVO.setAnswerTitle("test title1");
-        answerVO.setAnswerLikeCount(3);
+//        answerVO.setAnswerLikeCount(3);
         answerVO.setDoctorDepartmentType(DepartmentType.ENT_CLINIC.name());
         answerVO.setHospitalName("test hospital1");
         answerVO.setDoctorName("test doctor");
@@ -47,7 +47,7 @@ public class AnswerMapperTests {
         answerVO.setAnswerIsPublic(true);
         answerVO.setQuestionId(8L);
         answerVO.setAnswerTitle("test title1");
-        answerVO.setAnswerLikeCount(3);
+//        answerVO.setAnswerLikeCount(3);
         answerVO.setDoctorDepartmentType(DepartmentType.ENT_CLINIC.name());
         answerVO.setHospitalName("test hospital1");
         answerVO.setUserId(1L);
@@ -59,7 +59,7 @@ public class AnswerMapperTests {
         answerVO.setAnswerIsPublic(true);
         answerVO.setQuestionId(9L);
         answerVO.setAnswerTitle("test title1");
-        answerVO.setAnswerLikeCount(3);
+//        answerVO.setAnswerLikeCount(3);
         answerVO.setDoctorDepartmentType(DepartmentType.ENT_CLINIC.name());
         answerVO.setHospitalName("test hospital1");
         answerVO.setUserId(1L);
