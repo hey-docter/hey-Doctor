78696534f4ac10b266c53d44d022eeb4321d3085
README-wmoon <wmoon0024@gmail.com>
2023-05-28 14:50:32 +0900

    문우람 화면 목록

---
+++ src/main/java/com/heydoctor/app/domain/dto/Search.java
@@ -0,0 +1,16 @@
+package com.heydoctor.app.domain.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
+@NoArgsConstructor
+public class Search {
+    String keyword;
+
+    public Search(String keyword) {
+        this.keyword = keyword;
+    }
+}
